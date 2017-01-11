752c5c101c0aa6d27e261526f6d58ecbe58811be
fateh <fateh.khodier198@hotmail.com>
2017-01-11 15:14:05 +0100

    grades

--- code.c
+++ code.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-¨
-
-
-
-
-
+int main()
+{
+   int numberofgrades , i , grade;
+   int total=0;
+   float average ;
+   printf("enter how many grades\n");
+   scanf("%i",&numberofgrades);
+   return 0 ;
 }
